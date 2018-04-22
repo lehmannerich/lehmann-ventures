a2e31a9a299f46890be6a571fd2f4f1aedb00070
Ricky <lehmannerich@live.de>
2018-04-22 13:11:03 +0200

    Finalized slider & animation

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
-
-var vm1 = new Vue({
-  el: '#left',
-  data: {
-    msg: 'Hello World'
-  }
-});
-
-var vm2 = new Vue({
-  el: '#right',
-  data: {
-    msg: 'Hello World'
-  }
-});
+//
+// var vm1 = new Vue({
+//   el: '#left',
+//   data: { }
+// });
+//
+// var vm2 = new Vue({
+//   el: '#right',
+//   data: { },
+//   methods: {
+//
+//   }
+// });
 
 var slider = tns({
     container: '.my-slider',
@@ -21,7 +20,60 @@ var slider = tns({
     "mouseDrag": true,
     "autoHeight": true,
     slideBy: 'page',
-    "speed": 300,
+    "speed": 400,
     controls: false,
     nav: false
   });
+
+// Tiny Slider: https://github.com/ganlanyuan/tiny-slider
+
+
+var changeActiveState = function (info) {
+  console.log(info.index);
+  var index = info.index;
+  if (index === 1 || index === 5 || index === 9 || index === 13 || index === 17) {
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('#ec').classList.add('active');
+  }
+  else if (index === 2 ||index === 6 || index === 10 || index === 14 || index === 18) {
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('#tw').classList.add('active');
+  }
+  else if (index === 3 || index === 7 || index === 11 || index === 15 || index === 19) {
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('#ac').classList.add('active');
+  }
+  else if (index === 0 || index === 4 || index === 8 || index === 12 || index === 16) {
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('#lv').classList.add('active');
+  }
+
+}
+
+slider.events.on('indexChanged', changeActiveState);
+
+
+
+document.querySelector('#lv').onclick = function () {
+  slider.goTo(0);
+  document.querySelector('.active').classList.remove('active');
+  this.classList.add('active');
+};
+
+document.querySelector('#ec').onclick = function () {
+  slider.goTo(1);
+  document.querySelector('.active').classList.remove('active');
+  this.classList.add('active');
+};
+
+document.querySelector('#tw').onclick = function () {
+  slider.goTo(2);
+  document.querySelector('.active').classList.remove('active');
+  this.classList.add('active');
+};
+
+document.querySelector('#ac').onclick = function () {
+  slider.goTo(3);
+  document.querySelector('.active').classList.remove('active');
+  this.classList.add('active');
+};
